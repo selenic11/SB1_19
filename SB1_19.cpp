020422cbcb35ace50bb78081f7c81f219ab9246a
selenic11 <selenic11@users.noreply.github.com>
2021-08-02 22:38:44 +0300

    commit 1

---
+++ SB1_19.cpp
@@ -0,0 +1,63 @@
+﻿// SB1_19.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Animal {
+public:
+    virtual void Voice() {
+        cout << "Bu!\n";
+    }
+};
+
+class Dog : public Animal {
+public:
+    void Voice() override {
+        cout << "Woof!\n";
+    }
+};
+
+class Cat : public Animal {
+public:
+    void Voice() override {
+        cout << "Miau!\n";
+    }
+};
+
+class Duck : public Animal {
+public:
+    void Voice() override {
+        cout << "Krya!\n";
+    }
+};
+
+int main() {
+
+    vector<Animal*> vector_of_pointers;
+    vector_of_pointers.push_back(new Animal());
+    vector_of_pointers.push_back(new Dog());
+    vector_of_pointers.push_back(new Cat());
+    vector_of_pointers.push_back(new Duck());
+
+    for ( Animal *p : vector_of_pointers)
+    {
+        p->Voice();
+    }
+
+    return 0;
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
